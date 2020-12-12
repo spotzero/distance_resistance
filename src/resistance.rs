e676cce72526c1d124f17304c53b42b77a6c3042
David Pascoe-Deslauriers <dpascoed@coldfrontlabs.ca>
2020-12-12 13:17:04 -0500

    futher work on engine

--- src/resistance.rs
+++ src/resistance.rs
@@ -5,67 +5,27 @@ use rand::thread_rng;
 use rand::seq::SliceRandom;
 use crate::names::get_name;
 
-#[derive(Debug)]
-pub struct ResistanceGames {
-    pub rng: ThreadRng,
-    pub games: HashMap<String, ResistanceGame>,
-}
-
-#[derive(Debug)]
-pub struct ResistanceGame {
-    pub leader: usize,
-    pub players: HashMap<String, Player>,
-    pub spots: Vec<Spot>,
-    pub round: usize,
-    pub wins: usize,
-    pub status: Vec<RoundStatus>,
-    pub vote: usize,
-    pub started: bool,
-}
-
-#[derive(Debug)]
-#[derive(Default)]
-#[derive(Clone)]
+#[derive(Clone, Debug, Default)]
 pub struct RoundStatus {
     pub state: MissionState,
-    pub players: Vec<usize>,
+    pub operatives: Vec<usize>,
+    pub approvals: Vec<Approval>,
+    pub mission: Vec<bool>,
 }
 
-#[derive(Debug)]
-#[derive(Clone)]
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum Approval {
+    None,
+    Reject,
+    Approve,
+}
+
+#[derive(Clone, Debug)]
 pub struct Player {
     pub player_type: PlayerType,
     pub key: String,
     pub name: String,
     pub id: usize,
-}
-
-#[derive(Debug)]
-#[derive(Clone)]
-pub struct Spot {
-    pub key: String,
-    pub claimed: bool,
-}
-
-#[derive(Debug)]
-#[derive(Clone)]
-#[derive(PartialEq, Eq)]
-pub enum PlayerType {
-    Agent,
-    Spy,
-}
-
-#[derive(Debug)]
-#[derive(Clone)]
-#[derive(Copy)]
-#[derive(PartialEq, Eq)]
-pub enum MissionState {
-    Pending,
-    SelectingOperatives,
-    ApprovingMission,
-    RunningMission,
-    Failure,
-    Victory,
 }
 
 impl Player {
@@ -87,12 +47,50 @@ impl Player {
     }
 }
 
+#[derive(Clone, Debug)]
+pub struct Spot {
+    pub key: String,
+    pub claimed: bool,
+}
+
+#[derive(Clone, Debug)]
+pub struct ResistanceError;
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum PlayerType {
+    Agent,
+    Spy,
+}
+
 impl Default for PlayerType {
     fn default() -> Self { PlayerType::Agent }
 }
 
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+pub enum MissionState {
+    Pending,
+    SelectingOperatives,
+    ApprovingMission,
+    RunningMission,
+    Failure,
+    Victory,
+}
+
 impl Default for MissionState {
     fn default() -> Self { MissionState::Pending }
+}
+
+#[derive(Debug)]
+pub struct ResistanceGame {
+    pub leader: usize,
+    pub players: HashMap<String, Player>,
+    pub spots: Vec<Spot>,
+    pub round: usize,
+    pub wins: usize,
+    pub status: Vec<RoundStatus>,
+    pub vote: usize,
+    pub started: bool,
+    pub numberofplayers: usize,
 }
 
 impl ResistanceGame {
@@ -108,6 +106,7 @@ impl ResistanceGame {
                 vote: 0,
                 spots: spots,
                 started: false,
+                numberofplayers: numberofplayers,
             })
         } else {
             Err("Only 5 - 10 players are allowed")
@@ -142,7 +141,8 @@ impl ResistanceGame {
         for n in 0..self.spots.len() {
             if !self.spots[n].claimed {
                 self.spots[n].claimed = true;
-                self.players.get_mut(&self.spots[n].key).unwrap().id = n;
+                let player_key = self.spots[n].key.clone();
+                self.get_mut_player(&player_key)?.id = n;
                 return Ok(self.spots[n].key.clone());
             }
         }
@@ -164,36 +164,105 @@ impl ResistanceGame {
         }
     }
 
-    pub fn change_name(&mut self, player_key: String, name: String) -> Result<(), &'static str> {
+    pub fn change_name(&mut self, player_key: &String, name: String) -> Result<(), &'static str> {
         if self.started {
             Err("You cannot change your name once the game has started.")
         } else {
-            self.players.get_mut(&player_key).unwrap().change_name(name);
+            self.get_mut_player(&player_key)?.change_name(name);
             Ok(())
         }
     }
 
-    pub fn choose_operatives(&mut self, player_key: String, players: Vec<usize>) -> Result<(), &'static str> {
+    pub fn choose_operatives(&mut self, player_key: &String, selected_players: Vec<usize>) -> Result<(), &'static str> {
         if self.status[self.round].state != MissionState::SelectingOperatives {
             Err("It's not time to select operatives")
-        } else if self.leader != self.players.get(&player_key).unwrap().id {
+        } else if self.leader != self.get_player(&player_key)?.id {
           Err("You're not the leader!")
+        } else if mission_size(self.spots.len(), self.round) != selected_players.len() {
+            Err("Wrong number of operatives selected")
         } else {
+            self.status[self.round].operatives = selected_players;
+            self.status[self.round].state = MissionState::ApprovingMission;
             Ok(())
         }
     }
 
-    pub fn vote_to_approve(&mut self, player_key: String, vote: bool) -> Result<(), &'static str> {
-        Err("It's not time to vote!")
+    pub fn vote_to_approve(&mut self, player_key: &String, vote: bool) -> Result<(), &'static str> {
+        let playerid = self.playerid(player_key)?;
+        if self.status[self.round].state != MissionState::ApprovingMission {
+            Err("It's not time to vote!")
+        } else if self.has_voted(playerid) {
+            Err("Player has already voted!")
+        } else {
+            if self.status[self.round].approvals.len() == 0 {
+                self.status[self.round].approvals = vec![Approval::None; self.numberofplayers];
+            }
+            if vote {
+                self.status[self.round].approvals[playerid] = Approval::Approve;
+            } else {
+                self.status[self.round].approvals[playerid] = Approval::Reject;
+            }
+            if self.has_everyone_voted() {
+                self.approve_or_reject();
+            }
+
+            Ok(())
+        }
     }
 
-    pub fn succeed_mission(&mut self, player_key: String, status: bool) -> Result<(), &'static str> {
+    pub fn succeed_mission(&mut self, player_key: &String, status: bool) -> Result<(), &'static str> {
         Err("It's not even time for the mission")
     }
 
     fn current_status(&self) -> MissionState {
         self.status[self.round].state
     }
+
+    fn playerid(&self, player_key: &String) -> Result<usize, &'static str> {
+        Ok(self.get_player(player_key)?.id)
+    }
+
+    fn has_voted(&self, playerid: usize) -> bool {
+        !(self.status[self.round].approvals.len() == 0 || self.status[self.round].approvals[playerid] == Approval::None)
+    }
+
+    fn has_everyone_voted(&self) -> bool {
+        self.status[self.round].approvals.iter().filter(|a|**a == Approval::None).collect::<Vec<&Approval>>().len() == 0
+    }
+
+    fn approve_or_reject(&mut self) {
+        let approve = self.status[self.round].approvals.iter().filter(|a|**a == Approval::Approve).collect::<Vec<&Approval>>().len();
+        let reject = self.status[self.round].approvals.iter().filter(|a|**a == Approval::Reject).collect::<Vec<&Approval>>().len();
+        if approve >= reject {
+            self.status[self.round].state = MissionState::RunningMission;
+        } else {
+            self.vote += 1;
+            // Handle if vote fails
+            self.status[self.round].state = MissionState::RunningMission;
+        }
+    }
+
+    fn get_player(&self, player_key: &String) -> Result<&Player, &'static str> {
+        let player = self.players.get(player_key);
+        match player {
+            Some(p) => Ok(p),
+            None => Err("Invalid player"),
+        }
+    }
+
+    fn get_mut_player(&mut self, player_key: &String) -> Result<&mut Player, &'static str> {
+        let player = self.players.get_mut(player_key);
+        match player {
+            Some(p) => Ok(p),
+            None => Err("Invalid player"),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct ResistanceGames {
+    pub rng: ThreadRng,
+    pub games: HashMap<String, ResistanceGame>,
 }
 
 impl ResistanceGames {
@@ -210,12 +279,12 @@ impl ResistanceGames {
         Ok(id.clone())
     }
 
-    pub fn get(&self, key: String) -> Option<&ResistanceGame> {
-        self.games.get(&key)
+    pub fn get(&self, key: &String) -> Option<&ResistanceGame> {
+        self.games.get(key)
     }
 
-    pub fn get_mut(&mut self, key: String) -> Option<&mut ResistanceGame> {
-        self.games.get_mut(&key)
+    pub fn get_mut(&mut self, key: &String) -> Option<&mut ResistanceGame> {
+        self.games.get_mut(key)
     }
 }
 
